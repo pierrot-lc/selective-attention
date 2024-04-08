8177cefbd809952d031bc4c8237cb064d92e649f
Pierrot LC <pierrotlc@proton.me>
2024-04-08 16:51:58 +0200

    feat(trainer): eval metrics

--- main.py
+++ main.py
@@ -11,14 +11,15 @@ import src.trainer as trainer
 def main():
     dataset = ShakespearDataset.from_file(Path("./data/shakespear.txt"), 10)
     key = random.key(42)
-    batch_size = 2
+    batch_size = 64
+    n_iters = 10000
 
     key, sk = random.split(key)
     model = DecoderOnlyTransformer(
         dataset.vocab_size, 32, 2, 3, dataset.vocab_size, sk
     )
 
-    trainer.train(model, dataset, batch_size, key)
+    trainer.train(model, dataset, n_iters, batch_size, key)
 
 if __name__ == "__main__":
     main()
--- src/trainer.py
+++ src/trainer.py
@@ -1,28 +1,38 @@
+from collections import defaultdict
 from functools import partial
-from itertools import batched
 from typing import Iterable
 
 import equinox as eqx
 import jax
 import jax.numpy as jnp
 import jax.random as random
+import optax
 from beartype import beartype
 from jaxtyping import Array, Float, Int, jaxtyped
 from optax.losses import softmax_cross_entropy_with_integer_labels
-import optax
+from tqdm import tqdm
 
 from .datasets import ShakespearDataset
 from .model import DecoderOnlyTransformer
 
 
-def loader(dataset: Iterable, batch_size: int, key: random.PRNGKey):
+def loader(dataset: Iterable, batch_size: int, n_iters: int, key: random.PRNGKey):
     """Yield batches of samples from the dataset."""
-    sample_ids = random.permutation(key, len(dataset))
-
-    for batch_ids in batched(sample_ids, batch_size):
+    for sk in random.split(key, n_iters):
+        batch_ids = random.choice(sk, len(dataset), (batch_size,), replace=True)
         batch_samples = [dataset[id_] for id_ in batch_ids]
         batch_samples = jnp.stack(batch_samples)
         yield batch_samples
+
+
+def count_params(model: eqx.Module) -> Int[Array, ""]:
+    """Count the number of parameters of the given equinox module."""
+    params, _ = eqx.partition(model, eqx.is_array)
+
+    n_params = jax.tree.map(lambda p: jnp.prod(jnp.array(p.shape)), params)
+    n_params = jnp.array(jax.tree.leaves(n_params))
+    n_params = jnp.sum(n_params)
+    return n_params
 
 
 @jaxtyped(typechecker=beartype)
@@ -32,16 +42,66 @@ def loss_fn(
     static: eqx.Module,
     tokens: Int[Array, "batch_size seq_len"],
 ) -> Float[Array, ""]:
-    x, y = tokens[:, :-1], tokens[:, 1:]
+    """Compute the loss for the given batch of tokens."""
     model = eqx.combine(params, static)
+    x, y = tokens[:, :-1], tokens[:, 1:]
     y_logits = jax.vmap(model)(x)
     loss = softmax_cross_entropy_with_integer_labels(y_logits, y)
     return jnp.mean(loss)
 
 
+@jaxtyped(typechecker=beartype)
+@partial(jax.jit, static_argnums=1)
+def batch_metrics(
+    params: eqx.Module,
+    static: eqx.Module,
+    tokens: Int[Array, "batch_size seq_len"],
+) -> dict[str, Float[Array, ""]]:
+    """Compute the metrics for the given batch of tokens."""
+    metrics = dict()
+    model = eqx.combine(params, static)
+    x, y = tokens[:, :-1], tokens[:, 1:]
+    y_logits = jax.vmap(model)(x)
+
+    metrics["cross-entropy"] = softmax_cross_entropy_with_integer_labels(y_logits, y)
+    metrics["accuracy"] = y_logits.argmax(axis=2) == y
+
+    metrics = jax.tree_util.tree_map(jnp.mean, metrics)
+    return metrics
+
+
+def eval(
+    model: DecoderOnlyTransformer,
+    dataset: ShakespearDataset,
+    batch_size: int,
+    n_iters: int,
+    key: random.PRNGKey,
+) -> dict[str, Float[Array, ""]]:
+    """Evaluate the model and averages the metrics over the number of iterations."""
+    metrics = defaultdict(list)
+    params, static = eqx.partition(model, eqx.is_array)
+
+    dataloader = tqdm(
+        loader(dataset, batch_size, n_iters, key),
+        desc="Evaluating",
+        total=n_iters,
+        leave=False,
+    )
+
+    for batch in dataloader:
+        metrics_ = batch_metrics(params, static, batch)
+        for name, value in metrics_.items():
+            metrics[name].append(value)
+
+    metrics = {name: jnp.array(values) for name, values in metrics.items()}
+    metrics = jax.tree_util.tree_map(jnp.mean, metrics)
+    return metrics
+
+
 def train(
     model: DecoderOnlyTransformer,
     dataset: ShakespearDataset,
+    n_iters: int,
     batch_size: int,
     key: random.PRNGKey,
 ):
@@ -50,9 +110,23 @@ def train(
     optimizer = optax.adamw(1e-4)
     opt_state = optimizer.init(params)
 
-    for _ in range(1):
-        key, sk = random.split(key)
-        for batch in loader(dataset, batch_size, sk):
-            grads = grad_fn(params, static, batch)
-            updates, opt_state = optimizer.update(grads, opt_state, params)
-            params = optax.apply_updates(params, updates)
+    key, sk = random.split(key)
+    dataloader = tqdm(
+        loader(dataset, batch_size, n_iters, sk),
+        desc="Training",
+        total=n_iters,
+    )
+
+    n_params = count_params(model)
+    print(f"Number of parameters: {n_params:,}")
+
+    for iter_id, batch in enumerate(dataloader):
+        grads = grad_fn(params, static, batch)
+        updates, opt_state = optimizer.update(grads, opt_state, params)
+        params = optax.apply_updates(params, updates)
+
+        if iter_id % 100 == 0:
+            key, sk = random.split(key)
+            model = eqx.combine(params, static)
+            metrics = eval(model, dataset, batch_size, 10, key)
+            print(metrics)
